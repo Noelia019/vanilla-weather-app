db4995f80e572995d08dd9aad026b02b779cb7b3
Noelia019 <noeliaparina19@gmail.com>
2022-11-13 16:36:57 +0000

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+}
 
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bangkok&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
